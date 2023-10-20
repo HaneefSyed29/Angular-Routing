c364de80b00bdc58122355c8ab2b57a92d576f6d
HaneefSyed29 <shaneef2905@gmail.com>
2023-10-20 23:27:15 +0530

    Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { UserComponent } from './user/user.component';
+import { NoPageComponent } from './no-page/no-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'about',
+    component:AboutComponent
+  },
+  {
+    path:'home',
+    component:HomeComponent
+  },
+  {
+    path:'user/:id',
+    component:UserComponent
+  },
+  {
+    path:'**', //yahi hai wildcard
+    component:NoPageComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
